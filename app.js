79b416c571a89d9c92f7e505e6d4b3ec40a2d806
George Jacob <georgekunchattil@gmail.com>
2019-03-04 18:41:32 +0530

    Added use of '$injector'

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
     angular.module('DIApp', [])
     .controller('DIController', DIController);
 
-    function DIController ($scope, $filter) {  // filter is a service that let us create filtering functions that are used for formatting the data that eventually gets displayed to the user
+    function DIController ($scope, $filter, $injector) {  // filter is a service that let us create filtering functions that are used for formatting the data that eventually gets displayed to the user
         $scope.name ="Yaakov";
 
         $scope.upper = function () {
             var upCase =$filter('uppercase')
             $scope.name = upCase($scope.name);
         };
+
+        console.log($injector.annotate(DIController)); // $injector.annonate() gave us an array of the argument names to the function DIController
     }
 
     function AnnonateMe(name, job, blah) {
